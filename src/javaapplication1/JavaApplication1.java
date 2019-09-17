5218f87928cb5802cdb34c69a3cdf0e6b5b2db1d
pedago <pedago@pcalb-mm0617.univ-jfc.fr>
2019-09-17 16:27:15 +0200

    mise en place d'un syso : ok + 2

--- src/javaapplication1/JavaApplication1.java
+++ src/javaapplication1/JavaApplication1.java
@@ -15,7 +15,7 @@ public class JavaApplication1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hel0lo");
+        System.out.println("Hel01lo");
         System.out.println("OK");
     }
     
